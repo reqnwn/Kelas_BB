4092b881f1ac5c676c6aee4ce6106d18cd3488c7
reqnwn <aristyo42005@gmail.com>
2024-12-03 21:47:12 +0700

    versi 1

--- pertama.py
+++ pertama.py
@@ -40,3 +40,35 @@ data_panen={
         }
     }
 }
+
+print("\nSoal 1")
+for lokasi, detail in data_panen.items():
+    print(f"Nama Lokasi: {detail['nama_lokasi']}, Hasil Panen: {detail['hasil_panen']}")
+
+print("\nSoal 2")
+jumlah_jagung_lokasi2=data_panen["lokasi2"]["hasil_panen"]["jagung"]
+print(f"Jumlah hasil panen jagung di lokasi2: {jumlah_jagung_lokasi2}")
+
+print("\nSoal 3")
+nama_lokasi3=data_panen["lokasi3"]["nama_lokasi"]
+print(f"Nama lokasi 3: {nama_lokasi3}")
+
+print("\nSoal 4 & 5")
+hasil_padi={lokasi: detail['hasil_panen']['padi'] for lokasi,detail in data_panen.items()}
+hasil_keledai={lokasi: detail['hasil_panen']['kedelai'] for lokasi,detail in data_panen.items()}
+
+print("Jumlah hasil panen padi setiap lokasi: ")
+print(hasil_padi)
+
+print("\nJumlah hasil panen keledai setiap lokasi:")
+print(hasil_keledai)
+
+print("\nSoal 6")
+for lokasi, detail in data_panen.items():
+    padi=detail['hasil_panen']['padi']
+    jagung=detail['hasil_panen']['jagung']
+
+    if padi > 1300 or jagung > 800:
+        print(f"Lokasi {detail['nama_lokasi']} memerlukan perhatian khusus.")
+    else:
+        print(f"Lokasi {detail['nama_lokasi']} dalam kondisi baik.")
